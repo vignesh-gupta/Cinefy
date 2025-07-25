572ce08638aa2ecdf9850b9e4df7618900818472
Shaikh Abdul Rehman <atabdilrehman786@gmail.com>
2025-07-26 00:54:15 +0530

    add search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,13 +32,23 @@ function Home() {
 
     )
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault();
-        alert(searchQuery)
-        setSearchQuery("")
+        if (!searchQuery.trim()) return
+        if (loading) return
 
-
-    }
+        setLoading(true)
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (err) {
+            console.log(err)
+            setError("Failed to search movies....")
+        } finally {
+            setLoading(false)
+        }
+    };
 
     return (
         <div className="home">
